8d5e624945fadcd51423429d1b6b17b260ffd119
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-11-06 09:58:16 +0100

    add application, library and tailwindcss

---
+++ projects/ngx-datatable/src/lib/ngx-datatable.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-datatable',
+  imports: [],
+  template: `
+    <p>
+      ngx-datatable works!
+    </p>
+  `,
+  styles: ``,
+})
+export class NgxDatatable {
+
+}
---
+++ projects/ngx-datatable/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-datatable
+ */
+
+export * from './lib/ngx-datatable';
